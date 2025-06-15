ce27bb394b58ee8ae72563fef89084e661607a3e
j23161 越須賀　蓮 <j23161kr@edu.tuis.ac.jp>
2025-06-15 22:24:24 +0900

    add : 文章の追加

--- kadai6-1.py
+++ kadai6-1.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 
-APP_ID = "6c2c0b92e79b177a5dac164d5e6d7da4c9cb46dc"
-API_URL = "https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"
+APP_ID = "6c2c0b92e79b177a5dac164d5e6d7da4c9cb46dc"#自分のAPI
+API_URL = "https://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"#エンドポイント
 params = {
     "appId": APP_ID,
     "statsDataId": "0004009600",
@@ -22,10 +22,12 @@ data = res.json()["GET_STATS_DATA"]["STATISTICAL_DATA"]
 meta = data["CLASS_INF"]["CLASS_OBJ"]
 if isinstance(meta, dict):
     meta = [meta]
-
+    
+#データの取得
 values = data["DATA_INF"]["VALUE"]
 df = pd.DataFrame(values)
 
+#列のコードを名称に変換
 for c in meta:
     col_key = "@" + c["@id"]
     mapping = {}
@@ -36,6 +38,7 @@ for c in meta:
     if col_key in df.columns:
         df[col_key] = df[col_key].replace(mapping)
         
+#列名を日本語にする        
 col_map = {"@unit": "単位", "$": "値"}
 for c in meta:
     col_map["@"+c["@id"]] = c["@name"]
